711c3eb3fbf7a0d4264030f76280bf2634ac36d4
Badi James <CFPBTJ@PFR.CO.NZ>
2015-11-23 13:02:23 +1300

    Started on retrieving data source. Added comments

--- experimentlist/listretriever.py
+++ experimentlist/listretriever.py
@@ -3,18 +3,28 @@ import urllib, csv
 from experimentlist.models import Experiment
 from datetime import datetime
 
-"""Retrieves the experiment table from the database.
-
-Creates a Experiment model from each row
-"""
 
 file_name = "experi_list.csv"
 experi_table_url = "http://10.1.8.167:8000/report/experiment/csv/"
+data_source_url = "http://10.1.8.120:8000/report/data_source/csv/?experiment="
 
 
 def search_experiments(search_term):
-    # with urllib.request.urlopen(experi_table_url) as experi_csv:
-    with open(file_name) as experi_csv:
+    """Retrieves the experiment table from the genotype database.
+
+    Creates a Experiment model from each row where the name field
+    matches the given search term.
+    Does not save the models as the models should always be retrieved
+    from the genotype database, not the website DB. Instead returns a
+    list of the models
+
+    :param search_term String of name where the results must match
+    :return List of models.Experiment with name that matches search term.
+            None (instead of empty list) if no matching name field on
+            experiment table
+    """
+    with urllib.request.urlopen(experi_table_url) as experi_csv:
+    # with open(file_name) as experi_csv:
         results = _search_table(experi_csv, search_term)
     if len(results) == 0:
         return None
@@ -36,9 +46,17 @@ def _create_experiment(row):
     name = row['name']
     who = row['pi']
     when = row['createddate']
+    ds = _get_data_source(name)
     return Experiment(
         name=name, primary_investigator=who, date_created=when
     )
+
+
+def _get_data_source(name):
+    ds_url = data_source_url + name
+    with urllib.request.urlopen(ds_url) as ds:
+        # TODO: Do stuff. No idea what it looks like until server lets me in again
+        print("shalalala")
 
 
 def _format_time(date_string):
--- experimentlist/views.py
+++ experimentlist/views.py
@@ -7,6 +7,16 @@ from .listretriever import search_experiments
 
 
 def index(request):
+    """
+    Renders the search page according to the index.html template, with a
+    form.SearchForm as the search form.
+
+    If the search form has any GET data, retrieves models.Experiment that
+    match the GET data to populate a table
+
+    :param request:
+    :return:
+    """
     if request.method == 'GET' and 'search_field' in request.GET:
         form = SearchForm(request.GET)
         search_term = request.GET['search_field']
@@ -27,6 +37,18 @@ def index(request):
 
 
 def search(request, search_term):
+    """
+    Renders the search page according to the search.html template,
+    that is populating a table wit models.Experiments who's names
+    match the search_term parameter.
+    Called when url "~/experimentlist/<someExperimentName>/" requested
+
+    No longer used as using forms, but kept around for testing
+
+    :param request:
+    :param search_term: Name models.Experiment need to match to be in table
+    :return:
+    """
     search_list = Experiment.objects.filter(name=search_term)
     field_names = Experiment.field_names
     context = {
